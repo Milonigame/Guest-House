3f7db5a8591ab17ee8c65f670b9c53c630de1adb
Mariia <milonigame1@gmail.com>
2023-03-29 17:55:17 +0300

    ready tittle and room decor

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@ const TOKEN = "6190534304:AAExBy7_bP4uHfQlObJTrLU60CT-hCtkDcY",
      CHAT_ID = "-1001777196683",
      URL_API=`https://api.telegram.org/bot${ TOKEN }/sendMessage`,
      success=document.getElementById('success');
+     
 
 
 
 
 
-document.getElementById('tg').addEventListener('submit', function (e){
+ document.getElementById('tg').addEventListener('submit', function (e){
     e.preventDefault();
    
     let message =`<b>Заявка с сайта!</b>\n`;
@@ -40,3 +41,57 @@ document.getElementById('tg').addEventListener('submit', function (e){
         });
 
 });
+
+let slideIndex = 1;
+const slides = document.querySelectorAll('.offer__slide'),
+    prev = document.querySelector('.offer__slider-prev'),
+    next = document.querySelector('.offer__slider-next'),
+    total = document.querySelector('#total'),
+    current = document.querySelector('#current');
+
+showSlides(slideIndex);
+
+// if (slides.length < 10) {
+//     total.textContent = `0${slides.length}`;
+// } else {
+//     total.textContent = slides.length;
+// }
+
+function showSlides(n) {
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    function slideHide(){
+        slides.forEach(item =>{
+            item.classList.add('hide');
+            item.classList.remove('show');
+        });
+        }
+        slides.forEach((item) => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block'; // Как ваша самостоятельная работа - переписать на использование классов show/hide
+    
+   
+    if (slides.length < 10) {
+        current.textContent =  `0${slideIndex}`;
+    } else {
+        current.textContent =  slideIndex;
+    }
+}
+
+function plusSlides (n) {
+    showSlides(slideIndex += n);
+}
+
+prev.addEventListener('click', function(){
+    plusSlides(-1);
+});
+
+next.addEventListener('click', function(){
+    plusSlides(1);
+});
+
